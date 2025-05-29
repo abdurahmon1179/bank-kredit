f34821f76fafdd465e17ddf17feb1cd0fe76f9fd
abdurahmon1179 <sh5754020@gmail.com>
2025-05-29 17:50:01 +0500

    nine

---
+++ src/i18n/i18n.js
@@ -0,0 +1,18 @@
+
+
+import { createI18n } from 'vue-i18n'
+import uz from './uz.json'
+import ru from './ru.json'
+
+const messages = {
+  uz,
+  ru
+}
+
+const i18n = createI18n({
+  locale: 'uz',
+  fallbackLocale: 'ru',
+  messages,
+})
+
+export default i18n
